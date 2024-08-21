041937510b15aaadb7e827adc2718b0420c6b0b8
Ivan Tkachenko <frost19812014@gmail.com>
2024-08-21 11:55:28 +0300

    At last!

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -13,16 +13,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
     path(
-      'auth/registration/',
-      CreateView.as_view(
-          template_name='registration/registration_form.html',
-          form_class=UserCreationForm,
-          success_url=reverse_lazy('pages:homepage'),
-      ),
-      name='registration',
+        'auth/registration/',
+         CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+         ),
+        name='registration',
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler500 = 'pages.views.custom_500_error'
 handler404 = 'pages.views.page_not_found'
-handler403 = 'pages.views.csrf_failure'
