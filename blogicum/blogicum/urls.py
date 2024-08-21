063e12c26705415ac97e1af8ee4d6c98ff343a7f
Ivan Tkachenko <frost19812014@gmail.com>
2024-08-21 11:57:43 +0300

    At last!

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path(
         'auth/registration/',
-         CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('pages:homepage'),
+        CreateView.as_view(
+                template_name='registration/registration_form.html',
+                form_class=UserCreationForm,
+                success_url=reverse_lazy('pages:homepage'),
          ),
         name='registration',
     ),
